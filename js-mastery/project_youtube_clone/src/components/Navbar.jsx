beb9640e4bff6c086983e9db0c33e5c0ba770899
Aleksandar Ristic <storiaca@gmail.com>
2023-02-02 08:44:11 +0100

    Add components

--- js-mastery/project_youtube_clone/src/components/Navbar.jsx
+++ js-mastery/project_youtube_clone/src/components/Navbar.jsx
@@ -1,16 +1,9 @@
-import { Stack } from "@mui/material";
-import { Link } from "react-router-dom";
+import React from 'react'
 
-import { logo } from "../utils/constants";
-import { SearchBar } from "./";
+const Navbar = () => {
+  return (
+    <div><h3>Navbar</h3></div>
+  )
+}
 
-const Navbar = () => (
-  <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
-    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
-      <img src={logo} alt="logo" height={45} />
-    </Link>
-    <SearchBar />
-  </Stack>
-);
-
-export default Navbar;
+export default Navbar
