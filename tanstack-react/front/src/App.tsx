57b066c881cfa8c625397fb419649f5078fa9ad0
Aleksandar Ristic <storiaca@gmail.com>
2024-01-24 17:17:33 +0100

    Redux crud start

--- tanstack-react/front/src/App.tsx
+++ tanstack-react/front/src/App.tsx
@@ -1,5 +1,15 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 function App() {
-  return <>app</>;
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/todos')
+      .then((response) => setData(response.data))
+      .catch((err) => console.log(err));
+  }, []);
+  return <>{JSON.stringify(data)}</>;
 }
 
 export default App;
---
+++ tanstack-react/front/src/services/api.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import { Todo } from '../types/todo';
+
+const BASE_URL = 'http://localhost:8080';
+
+const axiosInstance = axios.create({ baseURL: BASE_URL });
+
+export const getTodosIds = async () => {
+  return (await axiosInstance.get<Todo[]>('todos')).data.map((todo) => );
+};
+// 7:54
