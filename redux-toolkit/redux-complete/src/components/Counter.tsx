01d14e67c49243e99bec6904de77aebf4a326579
Aleksandar Ristic <storiaca@gmail.com>
2024-05-02 13:11:41 +0200

    Create and configure store with typescript

---
+++ redux-toolkit/redux-complete/src/components/Counter.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Counter() {
+  return (
+    <div>
+      <h2>Count</h2>
+      <button>Plus</button>
+      <button>minus</button>
+    </div>
+  );
+}
+
+export default Counter;
