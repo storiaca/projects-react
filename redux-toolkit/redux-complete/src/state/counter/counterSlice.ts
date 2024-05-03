103c14050e668e300d6c26eb14b27f346f429baa
Aleksandar Ristic <storiaca@gmail.com>
2024-05-03 08:37:57 +0200

    Timestamp for redux toolkit video

---
+++ redux-toolkit/redux-complete/src/state/counter/counterSlice.ts
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface CounterState {
+  value: number;
+}
+
+const initialState: CounterState = {
+  value: 0,
+};
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {},
+});
+
+export default counterSlice.reducer;
+//export const { } counterSlice.actions;
--- redux-toolkit/redux-complete/src/state/store.ts
+++ redux-toolkit/redux-complete/src/state/store.ts
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterSlice from "./counter/counterSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    counterStore: counterSlice,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+// 14:22
