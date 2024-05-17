c01ba5f7173d69242196e2efda408653e72498d2
Aleksandar Ristic <storiaca@gmail.com>
2024-05-17 14:22:27 +0200

    Implement functions to Counter component

--- redux-toolkit/redux-complete/src/components/Counter.tsx
+++ redux-toolkit/redux-complete/src/components/Counter.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../state/store";
+import { increment, decrement } from "../state/counter/counterSlice";
 
 function Counter() {
+  const count = useSelector((state: RootState) => state.counterStore.value);
+  const dispatch = useDispatch()
   return (
     <div>
-      <h2>Count</h2>
-      <button>Plus</button>
-      <button>minus</button>
+      <h2 className="text-5xl my-3 text-red-600 font-medium">{count}</h2>
+      <button className="px-3 py-2 border mr-5" onClick={() => dispatch(increment())}>Increment</button>
+      <button className="px-3 py-2 border" onClick={() => dispatch(decrement())}>Decrement</button>
     </div>
   );
 }
--- redux-toolkit/redux-complete/src/state/counter/counterSlice.ts
+++ redux-toolkit/redux-complete/src/state/counter/counterSlice.ts
@@ -11,8 +11,15 @@ const initialState: CounterState = {
 const counterSlice = createSlice({
   name: "counter",
   initialState,
-  reducers: {},
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    }
+  },
 });
 
+export const { increment, decrement } = counterSlice.actions;
 export default counterSlice.reducer;
-//export const { } counterSlice.actions;
