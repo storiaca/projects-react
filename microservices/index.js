cd250fd4f0daf5f1450861002d891c727639162c
Aleksandar Ristic <storiaca@gmail.com>
2024-06-03 12:17:26 +0200

    Email seervice

--- microservices/index.js
+++ microservices/index.js
@@ -1,8 +1,10 @@
 import UserService from "./services/user.service.js";
+import EmailService from "./services/email.service.js";
 
 async function startApp() {
   // Start services
   await UserService.start();
+  await EmailService.start();
 
   try {
     // Simulate user creation
@@ -13,6 +15,13 @@ async function startApp() {
     console.log("New user created: ", newUser);
     const users = await UserService.call("user.getUsers");
     console.log("All users: ", users);
+    // Simulate sending email
+    const emailResult = await EmailService.call("email.sendEmail", {
+      recipient: newUser.email,
+      subject: "Welcome to our platform!",
+      content: "Thank you for singing up",
+    });
+    console.log(emailResult);
   } catch (error) {
     console.log("Error: ", error);
   } finally {
--- microservices/services/email.service.js
+++ microservices/services/email.service.js
@@ -1,3 +1,18 @@
 import { ServiceBroker } from "moleculer";
 
 const broker = new ServiceBroker();
+
+broker.createService({
+  name: "email",
+  actions: {
+    async sendEmail(ctx) {
+      const { recipient, subject, content } = ctx.params;
+      // Simulated email logic
+      console.log(`Sending email to ${recipient} with ${subject}`);
+      console.log(`Content: ${content}`);
+      return `Email sent to ${recepient}`;
+    },
+  },
+});
+
+export default broker;
