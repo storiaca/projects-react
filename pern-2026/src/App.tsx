6317a87e461d3a99e09bf2819a93eaf712ad6829
Aleksandar Ristic <storiaca@gmail.com>
2026-03-08 18:28:14 +0100

    Setup tailwind

--- pern-2026/src/App.tsx
+++ pern-2026/src/App.tsx
@@ -1,11 +1,21 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Onboarding from "./pages/Onboarding";
+import Profile from "./pages/Profile";
+import Account from "./pages/Account";
+import Auth from "./pages/Auth";
 function App() {
-
   return (
-    <>
-      <h1>Pern start</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/onboarding" element={<Onboarding />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/auth/:pathname" element={<Auth />} />
+        <Route path="/account/:pathname" element={<Account />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
