acf7caa8bb66b65c8747dda69e5a5b1080fd2888
Aleksandar Ristic <storiaca@gmail.com>
2026-03-16 11:54:18 +0100

    Set user in context

--- pern-2026/src/context/AuthContext.tsx
+++ pern-2026/src/context/AuthContext.tsx
@@ -1,3 +1,31 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState, type ReactNode } from "react";
+import type {User} from "../types"
+import { authClient } from "../lib/auth";
+
+interface AuthContextType {
+  user: User | null
+}
 
 const AuthContext = createContext<AuthContextType | null>(null)
+
+export default function AuthProvider({children}: {children: ReactNode}) {
+  const [neonUser, setNeonUser] = useState<any>(null)
+
+  useEffect(() => {
+    async function loadUser() {
+      try {
+        const result = await authClient.getSession()
+        if(result && result.data?.user) {
+          setNeonUser(result.data.user)
+        } else {
+          setNeonUser(null)
+        }
+      } catch (error) {
+        setNeonUser(null)
+      }
+    }
+
+    loadUser()
+  }, [])
+  return <AuthContext.Provider value={{user: neonUser}}>{children}</AuthContext.Provider>
+}
