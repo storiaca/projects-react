1e809e88ccee8f46ffabab51f08e0ba9f8c8c9da
Aleksandar Ristic <storiaca@gmail.com>
2026-03-30 12:10:14 +0200

    Onboarding start

--- pern-2026/src/pages/Onboarding.tsx
+++ pern-2026/src/pages/Onboarding.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import { RedirectToSignIn, SignedIn } from "@neondatabase/neon-js/auth/react";
 
 const Onboarding = () => {
-  return (
-    <div>
-      <h1>Onboarding</h1>
-    </div>
-  )
-}
+  const { user } = useAuth();
 
-export default Onboarding
+  if (!user) {
+    return <RedirectToSignIn />
+  }
+  return (
+    <SignedIn>
+      <div className="min-h-screen pt-24 pb-12 px-6">
+        <div className="max-w-xl mx-auto">
+          {/* Progrrss Indicator */}
+
+          {/* Step 1: Questionare */}
+          {/* Step 2: Generating */}
+        </div>
+      </div>
+    </SignedIn>
+  );
+};
+
+export default Onboarding;
--- pern-2026/src/pages/Profile.tsx
+++ pern-2026/src/pages/Profile.tsx
@@ -3,13 +3,13 @@ import { useAuth } from "../context/AuthContext"
 
 const Profile = () => {
   const {user, isLoading} = useAuth()
-  const plan = true
+  const plan = false
 
   if(!user && !isLoading) {
     return <Navigate to="/auth/sign-in" replace />
   }
 
-  if(plan) {
+  if(!plan) {
     return <Navigate to="/onboarding" replace />
   }
   return (
