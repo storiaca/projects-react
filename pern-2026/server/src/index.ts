28f9e0267d3e39763c7f1d34bf87734cd2d24e01
Aleksandar Ristic <storiaca@gmail.com>
2026-04-02 19:43:13 +0200

    Create plan and profile routes

--- pern-2026/server/src/index.ts
+++ pern-2026/server/src/index.ts
@@ -1,9 +1,20 @@
 import express from 'express';
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { planRouter } from './routes/plan';
+import { profileRouter } from './routes/profile';
 
 const app = express()
 const PORT = process.env.PORT || 3001
 
 app.use(cors())
 app.use(cookieParser())
+app.use(express.json())
+
+// API Routes
+app.use("/plan", planRouter);
+app.use("/profile", profileRouter)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`)
+})
